4ebb8dcc02efb232b550253cca2de81bb0f6411e
benhigh22 <benjaming.high@gmail.com>
2016-02-04 17:13:32 -0500

    Next save, working game

--- hangman_game.py
+++ hangman_game.py
@@ -1,27 +1,32 @@
 
+import random
 name = input("What is your name? ")
 
 print ("Hello, " + name, "Time to play hangman! You are allowed up to 8 wrong guesses.")
 
-random = "bathroom"
+random_file = "/usr/share/dict/words"
+WORDS = open(random_file).read().splitlines()
+random_word = random.choice(WORDS)
 counter = 0
 guesses = ""
-blanks = list("_" * len(random))
+blanks = list("_" * len(random_word))
 num_tries = 0
 print(blanks)
 
 while num_tries < 8:
+    if "_" not in blanks:
+        print ("You win!")
+        break
+
     guess = input("Choose a letter? ")
     counter = 0
-    for character in random:
+    for character in random_word:
         if character == guess:
             blanks[counter] = guess
-            print(counter)
         counter += 1
 
-    else:
-        print(blanks)
-
-    if guess not in random:
+    if guess not in random_word:
         num_tries += 1
-        print ("Wrong, guess again! " + "Failed attempt number " + str(num_tries))
+        print("Wrong, guess again! " + "Failed attempt number " + str(num_tries))
+    print(blanks)
+print("Sorry, you lose!")
